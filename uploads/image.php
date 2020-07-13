bd9896ac1cf708e555efd04d06d6377bc7778e54
John Andrés <johnandrescv@gmail.com>
2020-07-13 10:39:42 -0500

    Correccion de bugs en imagenes

--- uploads/image.php
+++ uploads/image.php
@@ -1,9 +1,5 @@
 <?php
+    header('Content-Type: image/jpeg'); 
     $name = $_GET['nombre'];
-    $dirname = "file:///media/Imagenes/";
-    $dir = "file:///media/Imagenes/".$name;
-    $image = glob($dirname.$name);
-    echo '<img src="'.$dir.'" /><br />';
-    echo $dir;
-    var_dump($image);
+    readfile("file:///media/Imagenes/".$name);
 ?>
